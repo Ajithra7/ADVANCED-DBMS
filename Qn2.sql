9ce97e41134e4619f5cc27f3338f422d70b3d4a1
Ajithra7 <79163509+Ajithra7@users.noreply.github.com>
2021-10-13 02:20:05 +0530

    Create Qn2.sql

---
+++ Qn2.sql
@@ -0,0 +1,3 @@
+Create table EMPLOYEE(EMPLOYEE_ID varchar(5) NOT NULL PRIMARY 
+KEY, EMP_FNAME varchar (20) NOT NULL, EMP_LNAME varchar(25) , 
+DEPARTMENT varchar (30) ,PJT varchar(30),ADDRESS varchar(30),DOB date, GENDER varchar (6)); 
